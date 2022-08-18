70349dc8cd63a5f44209303613551f2b54b4300b
agrad0 <grad.adam.j@gmail.com>
2022-08-18 23:04:33 +0200

    v1

---
+++ main.js
@@ -0,0 +1,6 @@
+let n = Number
+function myfunc(n){
+    if ((99 < n) && (n < 201))
+    return console.log("Liczba znajduje się w przedziale");
+    else return console.log("Liczba nie znajduje się w przedziale");
+}
