40f83c020d96d75ed37d454ff7c5fb1fbfa1734d
agrad0 <grad.adam.j@gmail.com>
2022-08-20 17:57:35 +0200

    v2

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-let n = Number
 function myfunc(n){
-    if ((99 < n) && (n < 201))
-    return console.log("Liczba znajduje się w przedziale");
-    else return console.log("Liczba nie znajduje się w przedziale");
+    let num = ("Liczba znajduje się w przedziale");
+    let notnum = ("Liczba nie znajduje się w przedziale");
+    if ((99 < n) && (n < 201)){
+    return num;
+    } else {return notnum;
+    }
 }
+myfunc(110);
+console.log(myfunc(80));
